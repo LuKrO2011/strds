3a7ce82c6ac23125c593db1cecc2a3fa5a226989
lukas <lukas.krodinger@gmail.com>
2025-05-05 10:29:33 +0200

    Adding run scripts for c module dataset creation

---
+++ scripts/top-c-modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the project root directory
+PROJECT_ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+# Mine the top 30 PyPI packages with C/C++ dependencies
+poetry run mine --use-top-packages --sample-size 30 --languages "C,C++" --csv-output "$PROJECT_ROOT_DIR/output/repos.csv"
+
+# Create a dataset using the correct filters
+poetry run dataset --csv-file "$PROJECT_ROOT_DIR/output/repos.csv" --tmp-dir "$PROJECT_ROOT_DIR/tmp" --filters "PrivateModuleFilter,TestModuleFilter,EmptyFilter" --output "$PROJECT_ROOT_DIR/output/dataset.json" --keep-tmp-dir
+
+# Create a Pynguin dataset
+poetry run provide pynguin --dataset "$PROJECT_ROOT_DIR/output/dataset.json" --output-path "$PROJECT_ROOT_DIR/output/c-modules.xml"
